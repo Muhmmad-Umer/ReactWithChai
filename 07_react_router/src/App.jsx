5ee7f787ead5d099264fe1cda42d8bf58a1cc8aa
Muhammad Umer <umertariq7a@gmail.com>
2024-06-20 16:10:25 +0500

    React Router

---
+++ 07_react_router/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
